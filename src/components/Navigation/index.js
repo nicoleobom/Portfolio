8a663d27021d12e6d6c09856235708f64c1be9b7
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-10-27 15:14:26 -0400

    changing href

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import './index.css';
 import PDF from '../../assets/NicoleOBomsawin_Resume2020.pdf'
 
+var hrefTag;
+
 export default class Navigation extends React.Component {
   componentDidMount() {
     window.addEventListener("scroll", this.stickyNav)
@@ -15,11 +17,14 @@ export default class Navigation extends React.Component {
     var sticky = header.offsetTop;
 
     if(window.pageYOffset > sticky) {
+      debugger;
       header.classList.add('sticky');
       homeBtn.classList.add('shown');
+      hrefTag = "#home"
     } else {
       header.classList.remove('sticky');
       homeBtn.classList.remove('shown');
+      hrefTag = "/"
     }
   }
 
@@ -28,7 +33,7 @@ export default class Navigation extends React.Component {
         <Row>
           <Col sm="12" className="header" id="header">
             <Nav className="nav-center">
-              <Nav.Link href="#home" className="nav-link hidden" id="home-btn">Home</Nav.Link>
+              <Nav.Link href="/" className="nav-link hidden" id="home-btn">Home</Nav.Link>
               <Nav.Link href="#projects" className="nav-link">Projects</Nav.Link>
               <Nav.Link href="#about" className="nav-link">About</Nav.Link>
               <Nav.Link href="mailto: nicoleobomsawin@gmail.com" className="nav-link">Contact</Nav.Link>
