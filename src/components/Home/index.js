4e2ab6935704fb07764bd12f13818efbbe36224c
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-10-26 20:40:29 -0400

    adjusting home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,15 +4,6 @@ import './index.css';
 import Navigation from '../Navigation/index';
 
 export default class Home extends React.Component {
-    toggleToast() {
-        const toast = document.getElementById('snackbar');
-        if(toast.style.visibility === "hidden") {
-            toast.style.visibility = "visible"
-        } else {
-            toast.style.visibility = "hidden"
-        }
-    }
-
     render() {
         return (
             <Row className="justify-content-md-center body" id="home">
@@ -21,7 +12,6 @@ export default class Home extends React.Component {
                     <h5>SOFTWARE DEVELOPER</h5>
                     <Navigation />
                 </Col>
-                
             </Row>
         )
     }
